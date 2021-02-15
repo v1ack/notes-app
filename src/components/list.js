97a8c15e295a85e845cf8dd9831008132a133965
vlack <kirilkin12@gmail.com>
2021-02-15 23:48:24 +0300

    NotesList animation

--- src/components/list.js
+++ src/components/list.js
@@ -2,14 +2,24 @@ import {Note} from "./note"
 import {useDispatch, useSelector} from "react-redux"
 import {add, selectNotes} from "../features/notesSlice"
 import circle from "./note/circle.svg"
+import {useTransition, animated} from "react-spring"
 
 export function NotesList(props) {
     const notes = useSelector(selectNotes)
 
+    const transitions = useTransition(notes.filter(props.filter), item => item.id, {
+        from: {opacity: 0, transform: "scale(0)"},
+        enter: {opacity: 1, transform: "scale(1)"},
+        leave: {opacity: 0, transform: "scale(0)"},
+        config: {
+            duration: 250,
+        },
+    })
+
     return <div className="NotesList">
-        {notes.filter(props.filter).map((note) =>
-            <Note {...note} key={note.id}/>)
-        }
+        {transitions.map(({item, props, key}) =>
+            <animated.div key={key} style={props}><Note {...item}/></animated.div>,
+        )}
     </div>
 }
 
